68ece2110c22c09b1805124af9a637897fb60c76
Ramon <ramondavi@msn.com>
2021-01-09 08:45:21 -0300

    deploy

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import store from '../redux/store';
 import { LOGOUT } from '../redux/user/user-types';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json'
   }
